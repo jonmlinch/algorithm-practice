d6be6c1f1be3937342419bafee48b2726b89d4ae
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-11-12 22:38:40 -0800

    added solution to determine if all letters in one array are present in another

---
+++ Easy/checkLetters.js
@@ -0,0 +1,13 @@
+//Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+function mutation(arr) {
+    let arr1 = arr[0].toLowerCase()
+    let arr2 = arr[1].toLowerCase()
+    for (let i = 0; i < arr2.length; i++){
+      if (arr1.indexOf(arr2[i]) < 0){
+        return false
+      }
+    }
+    return true;
+  }
+  
+  mutation(["Hello", "hey"]);
