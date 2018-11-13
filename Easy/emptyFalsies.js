5afbcca8240c86888dd307e5d9a69e01d0d30851
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-11-12 21:30:52 -0800

    add solution that removes falsy values from an array

---
+++ Easy/emptyFalsies.js
@@ -0,0 +1,12 @@
+//Remove all falsy values from an array.
+function bouncer(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i]){
+        newArr.push(arr[i])
+      }
+    }
+    return newArr;
+  }
+  
+  bouncer([7, "ate", "", false, 9]);
