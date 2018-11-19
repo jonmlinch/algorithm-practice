9a3bc448a381da4bb4a631fd7c2e93d02fe75f71
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-11-19 15:01:21 -0800

    more minor improvements

--- convertToSpinal.js
+++ convertToSpinal.js
@@ -1,2 +1,20 @@
 //Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
+function spinalCase(str) {
+    let newStr = str.replace(/^[A-Z]/, u => u.toLowerCase()).replace(/([A-Z])|(_)/g, " $1").replace(/(\s\s)|(\s)/g, "-")
+    console.log(newStr)
+    return newStr.toLowerCase() 
+    
+    return str;
+  }
+  
+  spinalCase('This Is Spinal Tap');
+  spinalCase("The_Andy_Griffith_Show")
+  spinalCase("thisIsSpinalTap")
+
+  //Better solution
+  function spinalCase(str) {
+    let newStr = str.replace(/([a-z])([A-Z])/g, "$1 $2").replace(/(_)|(\s)/g, "-")
+    console.log(newStr.toLowerCase())
+    return newStr.toLowerCase()
+  }
